c4cc204504c78ee557b0ab008402e2a263854705
Ayush Kumar <ayushsuperstar48@gmail.com>
2024-11-07 10:48:58 +0530

    .

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const http = require("http");
-const { Server } = require("socket.io"); // Importing the correct Socket.IO class
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 
-// Create a new instance of the Socket.IO server
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173", // Your frontend URL
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
-    credentials: true, // Enable if you need to pass credentials (like cookies)
+    credentials: true,
   },
 });
 
@@ -19,11 +18,27 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("New client connected");
 
-  socket.on("chatMessage", (message) => {
-    // Broadcast the message to all clients except the sender
-    socket.broadcast.emit("chatMessage", message);
+  // Joining a room based on the unique chatId
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+    console.log(`${socket.id} joined room: ${room}`);
   });
 
+  // Handle message sending
+  socket.on("chatMessage", (message) => {
+    io.to(message.chatId).emit("chatMessage", message); // Send message to everyone in the room
+  });
+
+  // Handle typing event
+  socket.on("typing", (data) => {
+    socket.broadcast.to(data.chatId).emit("typing", data);
+  });
+
+  socket.on("stopTyping", (data) => {
+    socket.broadcast.to(data.chatId).emit("stopTyping", data);
+  });
+
+  // Handle disconnection
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
